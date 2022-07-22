f09dbfe3fe002c4ab745b323966a8cabbf324318
Renad <renad.gharz@outlook.com>
2022-07-21 23:08:00 -0600

    converted to App class

--- app/gui.py
+++ app/gui.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 
 
-def main():
-	root = tk.Tk()
-	root.title("Convertsy")
-	root.geometry("4509x450")
-	root.mainloop()
+class App(tk.Tk):
+	def __init__(self):
+		super().__init__()
+		
+		self.title("Convertsy - Welcome!")
+		self.geometry("450x450")
+		self.resizable(False, False)
 	
 
 if __name__ == "__main__":
-	main()
+	app = App()
+	app.mainloop()
