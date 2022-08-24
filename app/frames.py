b44b4c1333ece4af538c9e13bcfa230dbfb1031d
Renad <renad.gharz@outlook.com>
2022-08-24 00:38:06 -0600

    button paddings

--- app/frames.py
+++ app/frames.py
@@ -5,20 +5,19 @@ class HomePage(ttk.Frame):
 	def __init__(self, master):
 		ttk.Frame.__init__(self, master)
 		
-		self.label = ttk.Label(self, text="Welcome to Convertsy! Get started below:")
-		self.label.pack()
+		ttk.Label(self, text="Welcome to Convertsy! Get started below:").pack(pady=(15, 15))
 
-		ttk.Button(self, text='Temperatures', width=25, command=lambda: master.change_frame(TemperaturePage)).pack()
+		ttk.Button(self, text='Temperatures', width=25, command=lambda: master.change_frame(TemperaturePage)).pack(pady=10)
 		
-		ttk.Button(self, text='Angle', width=25, command=lambda: master.change_frame(AnglePage)).pack()
+		ttk.Button(self, text='Angle', width=25, command=lambda: master.change_frame(AnglePage)).pack(pady=10)
 		
-		ttk.Button(self, text='Area', width=25, command=lambda: master.change_frame(AreaPage)).pack()
+		ttk.Button(self, text='Area', width=25, command=lambda: master.change_frame(AreaPage)).pack(pady=10)
 
-		ttk.Button(self, text='Currencies', width=25, command=lambda: master.change_frame(CurrencyPage)).pack()
+		ttk.Button(self, text='Currencies', width=25, command=lambda: master.change_frame(CurrencyPage)).pack(pady=10)
 
-		ttk.Button(self, text='Time', width=25, command=lambda: master.change_frame(CurrencyPage)).pack()
+		ttk.Button(self, text='Time', width=25, command=lambda: master.change_frame(CurrencyPage)).pack(pady=10)
 		
-		ttk.Button(self, text='Duration', width=25, command=lambda: master.change_frame(CurrencyPage)).pack()
+		ttk.Button(self, text='Duration', width=25, command=lambda: master.change_frame(CurrencyPage)).pack(pady=10)
 
 		self.pack()
 
