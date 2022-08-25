880fd664d1ab308729852da1eaa5a1a181815381
Renad <renad.gharz@outlook.com>
2022-08-24 19:55:19 -0600

    cleaned up frames

--- app/frames.py
+++ app/frames.py
@@ -12,12 +12,8 @@ class HomePage(ttk.Frame):
 		ttk.Button(self, text='Angle', width=25, command=lambda: master.change_frame(AnglePage)).pack(pady=10)
 		
 		ttk.Button(self, text='Area', width=25, command=lambda: master.change_frame(AreaPage)).pack(pady=10)
-
-		ttk.Button(self, text='Currencies', width=25, command=lambda: master.change_frame(CurrencyPage)).pack(pady=10)
-
-		ttk.Button(self, text='Time', width=25, command=lambda: master.change_frame(CurrencyPage)).pack(pady=10)
 		
-		ttk.Button(self, text='Duration', width=25, command=lambda: master.change_frame(CurrencyPage)).pack(pady=10)
+		ttk.Button(self, text='Duration', width=25, command=lambda: master.change_frame(DurationPage)).pack(pady=10)
 
 		self.pack()
 
@@ -43,21 +39,7 @@ class AreaPage(ttk.Frame):
 		ttk.Button(self, text='Home Page', width=25, command=lambda: master.change_frame(HomePage)).pack(pady=(15, 15))
 
 
-class CurrencyPage(ttk.Frame):
-	def __init__(self, master):
-		ttk.Frame.__init__(self, master)
-		
-		ttk.Button(self, text='Home Page', width=25, command=lambda: master.change_frame(HomePage)).pack(pady=(15, 15))
-
-
-class TimeZonePage(ttk.Frame):
-	def __init__(self, master):
-		ttk.Frame.__init__(self, master)
-		
-		ttk.Button(self, text='Home Page', width=25, command=lambda: master.change_frame(HomePage)).pack(pady=(15, 15))
-
-
-class Duration(ttk.Frame):
+class DurationPage(ttk.Frame):
 	def __init__(self, master):
 		ttk.Frame.__init__(self, master)
 		
